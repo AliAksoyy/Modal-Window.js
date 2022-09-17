cb6be0d84ad7b16110b8aaa7101fc664b4130d9d
Ali <alifeyza4017@gmail.com>
2022-09-17 09:15:45 +0300

    added

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
+
+// DOM ELEMENTS
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector("overlay")
 const btnCloseModal = document.querySelector(".close-modal")
 const btnsOpenModal = document.querySelectorAll(".show-modal")
 
 
+btnsOpenModal.forEach((i) => {
+    i.addEventListener("click", function() {
+        modal.classList.remove("hidden")
+        overlay.classList.remove("hidden")
+    })
+})
 
+btnCloseModal.addEventListener("click", function() {
+    modal.className = "hidden"
+    overlay.classList.add("hidden")
+})
